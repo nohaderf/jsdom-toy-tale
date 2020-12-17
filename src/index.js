f955342e9cc3ccc26fbf6dcace83801877709997
nohaderf <freda.hon@gmail.com>
2020-12-17 10:50:39 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+const divCollection = document.querySelector("#toy-collection")
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -14,91 +16,83 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
-function fetchToys(){
-  fetch('http://localhost:3000/toys')
-    .then(response => response.json())
-    .then(toy => renderAllToys(toy))
-}
-
-fetchToys()
-
-function renderAllToys(toyArray){
-  toyArray.forEach(renderToy)
-}
-
-const divCollection = document.querySelector("#toy-collection")
+fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(toysArray => toysArray.forEach(renderToy))
 
 function renderToy(toy){
   const divCard = document.createElement("div")
   divCard.className = "card"
   divCard.dataset.id = toy.id
-  divCard.innerHTML = `
-    <h2>${toy.name}</h2>
-    <img src=${toy.image} class="toy-avatar"/>
-    <p>${toy.likes}</p>
-    <button class="like-btn">Like <3</button>
-  `
-  divCollection.append(divCard)
- 
+
+  const h2 = document.createElement("h2")
+  h2.textContent = toy.name
+
+  const img = document.createElement("img")
+  img.src = toy.image
+  img.className = "toy-avatar"
+
+  const p = document.createElement("p")
+  p.textContent = toy.likes
+
+  const button = document.createElement("button")
+  button.className = "like-btn"
+  button.textContent = "Like <3"
+
+  // divCard.innerHTML = `
+  //   <h2>${toy.name}</h2>
+  //   <img src=${toy.image} class="toy-avatar" />
+  //   <p>${toy.likes}</p>
+  //   <button class="like-btn">Like <3</button>`
+
+    divCard.append(h2, img, p, button)
+    divCollection.append(divCard)
 }
-
-
-// ADD A NEW TOY
 
 const form = document.querySelector(".add-toy-form")
 
 form.addEventListener("submit", function(event){
   event.preventDefault()
   const name = event.target.name.value
-  const image =  event.target.image.value
-  // const likes = event.target.likes.value
+  const img = event.target.image.value
 
-  fetch('http://localhost:3000/toys', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': "application/json",
+  fetch(`http://localhost:3000/toys`, {
+    method: 'POST',
+    headers: {       
+      "Content-Type": "application/json",
+      Accept: "application/json"
   },
   body: JSON.stringify({
-    "name": name,
-    "image": image,
-    "likes": 0
-  }) 
-})
-  .then(response => response.json())
-  .then(data => {
-    renderToy(data)
+      name: name,
+      image: img,
+      likes: 0
   })
+  })
+  .then(response => response.json())
+  .then(data => renderToy(data)) 
 })
 
-// Add Likes
-
-
-  divCollection.addEventListener("click", function(event){
-
+divCollection.addEventListener("click", function(event){
     if (event.target.className === "like-btn"){
-      const likes = event.target.closest(".card")
-      const pLikes = likes.querySelector("p")
-      const totalLikes = parseInt(pLikes.textContent) + 1
-      pLikes.textContent = totalLikes
-      const id = likes.dataset.id
-      
-      return fetch(`http://localhost:3000/toys/${likes.dataset.id}`, {
-        method: "PATCH",
-        headers:{
+      const cardDiv = event.target.closest(".card")
+      const likes = cardDiv.querySelector("p")
+      const totalLikes = parseInt(likes.textContent) + 1
+      likes.textContent = totalLikes
+      const id = cardDiv.dataset.id
+      console.log(id)
+
+      fetch(`http://localhost:3000/toys/${id}`, {
+        method: 'PATCH',
+        headers: {       
           "Content-Type": "application/json",
-          "Accept": "application/json"
-        },  
-        body: JSON.stringify({
-          "likes": totalLikes,
-          "id": id
-        })
+          Accept: "application/json"
+      },
+      body: JSON.stringify({
+          likes: totalLikes
+      })
       })
       .then(response => response.json())
-      .then(data => {
-        console.log(data)
-      })
+      .then(data => console.log(data)) 
+
     }
-  })
-      
+})
